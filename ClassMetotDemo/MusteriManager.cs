9fd508759f7c89e33eb0b8aad1ad2fa7d0d49985
sevvalbrt <sevval.barut@hotmail.com>
2021-01-20 21:00:56 +0300

    ClassMetotDemo

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+        List<Musteri> musteriler = new List<Musteri>();
+
+        public void Ekle(Musteri musteri)
+        {
+            musteriler.Add(musteri);
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi+" isimli müşteri eklendi.");
+        }
+
+        
+        public void Listele()
+        {
+            
+            foreach (var musteri in musteriler)
+            {
+                
+                Console.WriteLine(musteri.Adi + " " + musteri.Soyadi);
+            }
+            
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            musteriler.Remove(musteri);
+            Console.WriteLine("\nSilinen Müşteri: " + musteri.Adi + " " + musteri.Soyadi);
+            Console.WriteLine("\nGüncel Müşteri Listesi");
+            Console.WriteLine("----------------------");
+            Listele();
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Şevval";
+            musteri1.Soyadi = "Barut";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Meral";
+            musteri2.Soyadi = "Taşdemir";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Adi = "Ayşe";
+            musteri3.Soyadi = "Elgören";
+
+
+            MusteriManager manager = new MusteriManager();
+            manager.Ekle(musteri1);
+            manager.Ekle(musteri2);
+            manager.Ekle(musteri3);
+
+            Console.WriteLine("\nMüşteri Listesi");
+            Console.WriteLine("---------------");
+            manager.Listele();
+
+            manager.Sil(musteri1);
+            Console.ReadLine();
+
+        }
+    }
+}
